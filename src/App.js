3cabcde66a65e17a30fad6798789fb7eee389729
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-23 11:15:44 +0530

    built the static part - forms

--- src/App.js
+++ src/App.js
@@ -27,7 +27,10 @@ function App() {
     <div className='app'>
       <div className='sidebar'>
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -66,8 +69,53 @@ const Friend = ({friend}) => {
           <p>You and {friend.name} are even</p>
         }
 
-        <button className='button'>Select</button>
+        <Button>Select</Button>
       </li>
     </Fragment>
   )
 };
+
+const Button = ({children}) => {
+  return (
+    <button className='button'>{children}</button>
+  );
+}
+
+const FormAddFriend = () => {
+  return (
+    <form className='form-add-friend'>
+      <label>👫Friendname</label>
+      <input type='text' />
+
+      <label>🌇 Image URL</label>
+      <input type='text' />
+
+      <Button>Add</Button>
+    </form>
+  )
+}
+
+const FormSplitBill = () => {
+  return (
+    <form className='form-split-bill'>
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type='text' />
+
+      <label>🕴️Your expense</label>
+      <input type='text' />
+
+      <label>👫 X's expense</label>
+      <input type='text' disabled />
+
+      <label>🤑 who is paying the bill</label>
+      <select>
+        <option value='user'>You</option>
+        <option value='friend'>X</option>
+      </select>
+
+      <Button>Add</Button>
+    </form>
+  );
+}
