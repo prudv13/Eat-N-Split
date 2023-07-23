4e025146c34598314f5b720ef24c3f27b9ee2c00
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-23 10:48:55 +0530

    built static part - list of friends

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
+import { Fragment } from 'react';
 import './App.css';
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
 function App() {
   return (
-    <div>
-      <h1>Eat-'N-Split</h1>
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendsList />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const FriendsList = () => {
+
+  const friends = initialFriends;
+
+  return (
+    <Fragment>
+      <ul>
+      {friends.map(friend => <Friend friend={friend} key={friend.id} />)}
+      </ul>
+    </Fragment>
+  )
+}
+
+const Friend = ({friend}) => {
+  return (
+    <Fragment>
+      <li>
+        <img src={friend.image} alt={friend.name} />
+        <h3>{friend.name}</h3>
+
+        {friend.balance < 0 && 
+          <p className='red'>You owe {friend.name} {Math.abs(friend.balance)}$</p>
+        }
+
+        {friend.balance > 0 && 
+          <p className='green'>{friend.name} owes you {Math.abs(friend.balance)}$</p>
+        }
+
+        {friend.balance=== 0 && 
+          <p>You and {friend.name} are even</p>
+        }
+
+        <button className='button'>Select</button>
+      </li>
+    </Fragment>
+  )
+};
